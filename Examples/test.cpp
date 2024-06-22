d359bc0e6379ad1b4b0c5313ecf48705695867b8
Neko Box Coder <nekoboxcoder@gmail.com>
2024-06-22 20:48:48 +0100

    Updating examples

--- Examples/test.cpp
+++ Examples/test.cpp
@@ -6,35 +6,12 @@ Dependencies:
     Source:
         Type: Git
         Value: "https://github.com/Neko-Box-Coder/ssLogger.git"
-    # LibraryType: Static
     LibraryType: Header
     IncludePaths:
         - "Include"
     
-    # (Optional if LibraryType is Header) Link properties of the dependency
-    # LinkProperties:
-    #     # Properties for searching the library binary for the profile
-    #     "g++":
-    #         # The library names to be searched for when linking against the script
-    #         SearchLibraryNames: ["ssLogger"]
-    #         
-    #         # (Optional) The library names to be excluded from being searched
-    #         ExcludeLibraryNames: ["ssLogger_SRC"]
-    #         
-    #         # The path (relative to the dependency folder) to be searched for the dependency binaries
-    #         SearchDirectories: ["./build"]
-    #         
-    #         # (Optional) Additional link options for this dependency
-    #         AdditionalLinkOptions: 
-    #             All: []
-
-    
-    # (Optional) List of setup commands for the supported platforms
     Setup:
-        # Setup commands for the specified platform
         All:
-            # Setup commands for the specified profile. 
-            # All commands are run in the dependency folder
             "g++":
             -   "git submodule update --init --recursive"
             -   "mkdir build"
@@ -43,13 +20,7 @@ Dependencies:
 */
 
 
-#include "ssLogger/ssLogSwitches.hpp"
-
-#define ssLOG_USE_SOURCE 0
-#if !ssLOG_USE_SOURCE
-    #include "ssLogger/ssLogInit.hpp"
-#endif
- 
+#include "ssLogger/ssLogInit.hpp"
 #include "ssLogger/ssLog.hpp"
 
 #include <iostream>
@@ -75,13 +46,11 @@ int main(int argc, char* argv[])
     ssLOG_INFO("Test info: " << CheckCounter);
     ssLOG_DEBUG("Test debug: " << CheckCounter);
  
-    for(int i = 0; i < 10000; ++i)
+    for(int i = 0; i < 100; ++i)
     {
         ssLOG_LINE("Logging test: " << i);
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
-    
-    
     
     return 0;
 }
--- Examples/testSDL.cpp
+++ Examples/testSDL.cpp
@@ -1,38 +1,11 @@
 /* runcpp2
 
-# (Optional) The list of dependencies needed by the script
-Dependencies:
-    # Dependency name
--   Name: SDL2
-    
-    # Supported platforms of the dependency
-    Platforms: [Unix]
-    
-    # The source of getting the dependency
-    Source:
-        Type: Local
-        Value: "./dummy"
-    
-    # Library Type (Static, Object, Shared, Header)
-    LibraryType: Shared
-    
-    # (Optional if LibraryType is Header) Link properties of the dependency
-    LinkProperties:
-        # Properties for searching the library binary for the profile
+OverrideLinkFlags:
+    All:
         "g++":
-            # The library names to be searched for when linking against the script
-            SearchLibraryNames: []
-            
-            # (Optional) The library names to be excluded from being searched
-            ExcludeLibraryNames: []
-            
-            # The path (relative to the dependency folder) to be searched for the dependency binaries
-            SearchDirectories: []
-            
-            # (Optional) Additional link options for this dependency
-            AdditionalLinkOptions: 
-                # Applies to all platforms (Can be changed to Linux, MacOS, Windows, Unix, etc.)
-                Unix: ["-lSDL2"]
+            Remove: ""
+            Append: "-lSDL2"
+
 */
 
 
@@ -48,20 +21,19 @@ int main(int arc, char ** argv)
 
     SDL_Window* window = nullptr;
     
-    if (SDL_Init( SDL_INIT_VIDEO ) < 0) {
+    if (SDL_Init( SDL_INIT_VIDEO ) < 0)
         std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-    } else {
-        
-        window = SDL_CreateWindow(
-            "SDL2 Demo",
-            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-            SCREEN_WIDTH, SCREEN_HEIGHT,
-            SDL_WINDOW_SHOWN
-        );
+    else
+    {
+        window = SDL_CreateWindow(  "SDL2 Demo",
+                                    SDL_WINDOWPOS_CENTERED, 
+                                    SDL_WINDOWPOS_CENTERED,
+                                    SCREEN_WIDTH, 
+                                    SCREEN_HEIGHT,
+                                    SDL_WINDOW_SHOWN);
     }
     
     bool quit = false;
-    
     SDL_Event e;
     
     while (!quit) 
@@ -74,8 +46,6 @@ int main(int arc, char ** argv)
     }
     
     SDL_DestroyWindow(window);
-    
     SDL_Quit();
-    
     return 0;
 }
