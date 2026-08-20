c9c23a3c234fb7c20682e4a0e7455d307f8e243b
anon <anon@example.com>
2026-08-20 18:14:50 +0100

    WIP 2

--- Src/runcpp2/Data/DependencyInfo.hpp
+++ Src/runcpp2/Data/DependencyInfo.hpp
@@ -4,6 +4,7 @@
 #include "runcpp2/Data/DependencyLibraryType.hpp"
 #include "runcpp2/Data/DependencySource.hpp"
 #include "runcpp2/Data/DependencyLinkProperty.hpp"
+#include "runcpp2/Data/DependencyCompileProperty.hpp"
 #include "runcpp2/Data/ProfilesCommands.hpp"
 #include "runcpp2/Data/ParseCommon.hpp"
 #include "runcpp2/Data/ParameterValue.hpp"
@@ -36,6 +37,7 @@ namespace Data
         std::vector<std::string> IncludePaths;
         std::vector<std::string> AbsoluteIncludePaths;
         std::unordered_map<PlatformName, DependencyLinkProperty> LinkProperties;
+        std::unordered_map<PlatformName, DependencyCompileProperty> CompileProperties;
         std::unordered_map<PlatformName, ProfilesCommands> Setup;
         std::unordered_map<PlatformName, ProfilesCommands> Cleanup;
         std::unordered_map<PlatformName, ProfilesCommands> Build;
@@ -91,6 +93,9 @@ namespace Data
                 
                 //Expecting either platform profile map or ProfileLinkProperty map
                 NodeRequirement("LinkProperties", YAML::NodeType::Map, false, false),
+                
+                //Expecting either platform profile map or ProfileCompileProperty map
+                NodeRequirement("CompileProperties", YAML::NodeType::Map, false, false),
                 
                 //Setup can be platform profile map or sequence of commands, handle later
                 //Cleanup can be platform profile map or sequence of commands, handle later
@@ -148,6 +153,17 @@ namespace Data
             {
                 return DS_ERROR_MSG("DependencyInfo: Missing LinkProperties with library type " + 
                                     Data::DependencyLibraryTypeToString(LibraryType));
+            }
+
+            if(ExistAndHasChild(clonedNode, "CompileProperties"))
+            {
+                DS_ASSERT_TRUE( ParsePlatformProfileMap<DependencyCompileProperty>
+                                (
+                                    clonedNode, 
+                                    "CompileProperties", 
+                                    CompileProperties, 
+                                    "CompileProperties"
+                                ));
             }
 
             DS_ASSERT_TRUE(ParsePlatformProfileMap<ProfilesCommands>(   clonedNode, 
